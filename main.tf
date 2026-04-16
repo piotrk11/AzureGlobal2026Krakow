1a72cb2011ac61a2c0de30f3c6bf18895f6b5651
Piotr Kukula <piotr.kukula@uken.krakow.pl>
2026-04-16 21:27:48 +0200

    Add container registry module to main.tf

--- main.tf
+++ main.tf
@@ -92,3 +92,13 @@ module "application_insights" {
     location = "polandcentral"    
   }
 }
+
+module "container_registry" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=container_registry/v1.0.0"
+  # also any inputs for the module (see below)
+  container_registry_name = "container-registry-name-user12"
+  resource_group = {
+    name = "rg-user12"
+    location = "polandcentral"    
+  }
+}
