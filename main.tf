843940cba868e538272912e47a5b1886c8d1a231
Piotr Kukula <piotr.kukula@uken.krakow.pl>
2026-04-16 20:43:44 +0200

    Add MSSQL server module to main.tf
    Added MSSQL server module configuration with parameters.

--- main.tf
+++ main.tf
@@ -27,9 +27,19 @@ module "keyvault" {
     name = "rg-user12"
     location = "polandcentral"    
   }
-
   network_acls = {
     bypass = "AzureServices"
   }
+}
 
+module "mssql_server" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=mssql_server/v1.0.0"
+  # also any inputs for the module (see below)
+  resource_group = {
+    name = "rg-user12"
+    location = "polandcentral"    
+  }
+  sql_server_admin = "mssqladmin1"
+  sql_server_name = "mssqlname1"
+  sql_server_version = "mssqlversion1"  
 }
